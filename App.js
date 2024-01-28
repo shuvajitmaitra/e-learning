ad1711da3ab5ecc3a903bbe70fdb9dae26022d59
Shuvajit Maitra <shuvajitmaitra@gmail.com>
2024-01-28 09:04:28 +0600

    Add custom fonts

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import LoginScreen from "./Apps/Screens/LoginScreen";
+import { useFonts } from 'expo-font';
+
 
 export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    "Inter-Regular": require("./assets/Fonts/Inter-Regular.ttf"),
+    "Inter-Medium": require("./assets/Fonts/Inter-Medium.ttf"),
+    "Inter-Thin": require("./assets/Fonts/Inter-Thin.ttf"),
+    "Inter-Bold": require("./assets/Fonts/Inter-Bold.ttf"),
+  });
+
   return (
     <View style={styles.container}>
       <Text>Hello world</Text>
-      <Text>Hello Mitul Bhai</Text>
+      <Text style={{fontFamily: "Inter-Medium"}}>E-Learning</Text>
       <LoginScreen />
       <StatusBar style="auto" />
     </View>
@@ -19,5 +28,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+   
+  
   },
 });
